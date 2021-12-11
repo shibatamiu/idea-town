1f664f6db0b2761559f11adf725d80a596c0d384
miu <miu@miunoMacBook-Pro.local>
2021-12-11 18:25:15 +0900

    ○○.index.erbを修正

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -5,7 +5,7 @@ class ThoughtsController < ApplicationController
         
         if params[:search] != nil && params[:search] != ''
             search = params[:search]
-            @thoughts = Thought.joins(:user).order("created_at DESC").where("title LIKE ? OR about LIKE ? OR name LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%").page(params[:page]).per(3)
+            @thoughts = Thought.joins(:user).order(id: "DESC").where("title LIKE ? OR about LIKE ? OR name LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%").page(params[:page]).per(3)
         else
             @thoughts = params[:tag_id].present? ? Tag.find(params[:tag_id]).thoughts.order(id: "DESC").page(params[:page]).per(3) : Thought.all.order(id: "DESC").page(params[:page]).per(3)
         end
