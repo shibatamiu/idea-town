6b301e02195885b644c339ec56b23830e381d554
miu <miu@miunoMacBook-Pro.local>
2021-12-27 05:56:26 +0900

    ○○.index.erbを修正

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,12 +1,8 @@
 class RanksController < ApplicationController
 
     def rank
-        if params[:search] != nil && params[:search] != ''
-            search = params[:search]
-            @thoughts = Thought.joins(:user).order(id: "DESC").where("title LIKE ? OR about LIKE ? OR name LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%").page(params[:page]).per(3)
-        else
-            @thoughts = params[:tag_id].present? ? Tag.find(params[:tag_id]).thoughts.order(id: "DESC").page(params[:page]).per(3) : Thought.all.order(id: "DESC").page(params[:page]).per(3)
-        end
-        @rank_thoughts = params[:tag_id].present? ? Tag.find(params[:tag_id]).thoughts.page(params[:page]).per(3).sort {|a,b| b.liked_users.count <=> a.liked_users.count} : Thought.all.page(params[:page]).per(3).sort {|a,b| b.liked_users.count <=> a.liked_users.count}
+        @rank_thoughts = params[:tag_id].present? ? Tag.find(params[:tag_id]).thoughts.sort {|a,b| b.liked_users.count <=> a.liked_users.count} : Thought.all.sort {|a,b| b.liked_users.count <=> a.liked_users.count}
+        @rank_thoughts = Kaminari.paginate_array(@rank_thoughts).page(params[:page]).per(5)
+        
     end
 end
